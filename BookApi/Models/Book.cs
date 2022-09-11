413bc96be79ab643c600a80387c1ae345bc40275
RosarioRu <mariarruval@gmail.com>
2022-09-11 16:11:30 -0700

    Update models.

--- BookApi/Models/Book.cs
+++ BookApi/Models/Book.cs
@@ -1,13 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookApi.Models
 {
     public class Book
     {
         public int BookId { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Reviews { get; set; }
         
-        public string Rating { get; set; }
+        [Required]
+        public int Rating { get; set; }
     }
 }
