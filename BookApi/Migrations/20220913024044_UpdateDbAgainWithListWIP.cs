2dca31cd21f2587accd8853ac6d633fd63c02e10
RosarioRu <mariarruval@gmail.com>
2022-09-12 20:33:19 -0700

    WIP: list of ratings.

---
+++ BookApi/Migrations/20220913024044_UpdateDbAgainWithListWIP.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookApi.Migrations
+{
+    public partial class UpdateDbAgainWithListWIP : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
