771bcdc17e7e055a1fb9ed9d9ef7ff187b81987e
RosarioRu <mariarruval@gmail.com>
2022-09-12 19:12:21 -0700

    Add list property, WIP: trying to get a running list of submittted ratings.

--- BookApi/Models/Book.cs
+++ BookApi/Models/Book.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic; 
 
 namespace BookApi.Models
 {
@@ -7,12 +8,35 @@ namespace BookApi.Models
         public int BookId { get; set; }
 
         [Required]
-        public string Title { get; set; }
+        public string Title { get; init; }
 
         [Required]
         public string Reviews { get; set; }
         
+        // [Required]
+        // public int Rating { get; set; }
         [Required]
-        public int Rating { get; set; }
+        public int SingleRating {get; set;} //hidden/not displayed to user?
+
+        public List<int> AllRatings {get 
+          { 
+            List<int> theRatings = new List<int>();
+            theRatings.Add(SingleRating);
+            return theRatings;
+
+            // return 1+ SingleRating;
+          }
+        }
+
+        //  public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+        // public string FullName => $"{FirstName} {LastName}";
+
+        // public List<int> AllRatings 
+        // {
+        //   AllRatings.Add( $"{SingleRating}" );
+        //   get{ return $"{AllRatings}"}; 
+        // }
+
     }
 }
